95e837ee986bff0259a09a64d0c90189c570c9f5
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-07-31 16:23:17 -0400

    basic set up of index and app.js, installed webpack and node

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.clasName = 'container'
 root.render(
   <React.StrictMode>
     <App />
